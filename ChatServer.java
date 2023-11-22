bf6084e63460669547e198c0c41164861515faa0
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-11-22 22:48:31 +0300

    updating the leaving and error messages

--- ChatServer.java
+++ ChatServer.java
@@ -1,177 +1,133 @@
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.Set;
-import java.util.HashSet;
 import java.util.Scanner;
-import java.util.concurrent.Executors;
+
+import java.awt.BorderLayout;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
 
 /**
- * A multithreaded chat room server. When a client connects the server requests
- * a screen name by sending the client the text "SUBMITNAME", and keeps
- * requesting a name until a unique one is received. After a client submits a
- * unique name, the server acknowledges with "NAMEACCEPTED". Then all messages
- * from that client will be broadcast to all other clients that have submitted a
- * unique screen name. The broadcast messages are prefixed with "MESSAGE".
+ * A simple Swing-based client for the chat server. Graphically it is a frame
+ * with a text field for entering messages and a textarea to see the whole
+ * dialog.
  *
- * This is just a teaching example so it can be enhanced in many ways, e.g.,
- * better logging. Another is to accept a lot of fun commands, like Slack.
+ * The client follows the following Chat Protocol. When the server sends
+ * "SUBMITNAME" the client replies with the desired screen name. The server will
+ * keep sending "SUBMITNAME" requests as long as the client submits screen names
+ * that are already in use. When the server sends a line beginning with
+ * "NAMEACCEPTED" the client is now allowed to start sending the server
+ * arbitrary strings to be broadcast to all chatters connected to the server.
+ * When the server sends a line beginning with "MESSAGE" then all characters
+ * following this string should be displayed in its message area.
  */
-public class ChatServer {
+public class ChatClient {
 
-    // These sets are used for managing client connections and broadcasting messages.
-    // All client names, so we can check for duplicates upon registration.
-    private static Set<String> names = new HashSet<>();
+    String serverAddress;
+    // Scanner for reading input from the server,
+    // a PrintWriter for sending output to the server,
+    Scanner in;
+    PrintWriter out;
 
-    // The set of all the print writers for all the clients, used for broadcast.
-    private static Set<PrintWriter> writers = new HashSet<>();
-
-    private static String serverGroupName ;
-
-    public static String getGroupName() {
-        return serverGroupName;
-    }
-    
-    public static void setGroupName(String groupName) {
-        ChatServer.serverGroupName = groupName;
-    }
-    public static void main(String[] args) throws Exception {
-        System.out.println("The chat server is running...");
-        var pool = Executors.newFixedThreadPool(500);
-
-        //try-with-resources statement in Java. 
-        // It is a convenient way to ensure that a resource is properly closed after being used, 
-        // even if an exception occurs.
-        try (var listener = new ServerSocket(59001)) {
-            while (true) {
-
-                // Whenever a client connects, a new Handler thread is created
-                // and added to the thread pool to handle the client's requests.
-                pool.execute(new Handler(listener.accept()));
-            }
-        }
-    }
+    // Swing components for the GUI
+    JFrame frame = new JFrame("Chatter");
+    JTextField textField = new JTextField(50);
+    JTextArea messageArea = new JTextArea(16, 50);
 
     /**
-     * The client handler task.
-     * Each Handler instance is responsible for handling a single client connection.
+     * Constructs the client by laying out the GUI and registering a listener with
+     * the textfield so that pressing Return in the listener sends the textfield
+     * contents to the server. Note however that the textfield is initially NOT
+     * editable, and only becomes editable AFTER the client receives the
+     * NAMEACCEPTED message from the server.
      */
-    private static class Handler implements Runnable {
-        private String name;
-        private String clientGroupName ;
-        private Socket socket;
-        private Scanner in;
-        private PrintWriter out;
+    public ChatClient(String serverAddress) {
+        this.serverAddress = serverAddress;
 
-        /**
-         * Constructs a handler thread, squirreling away the socket. All the interesting
-         * work is done in the run method. Remember the constructor is called from the
-         * server's main method, so this has to be as short as possible.
-         */
-        public Handler(Socket socket) {
-            this.socket = socket;
-        }
+        // initializes the GUI components
+        // sets the text field as initially not editable, 
+        
+        textField.setEditable(false);
+        messageArea.setEditable(false);
+        frame.getContentPane().add(textField, BorderLayout.SOUTH);
+        frame.getContentPane().add(new JScrollPane(messageArea), BorderLayout.CENTER);
+        frame.pack();
+        
+        // and registers an action listener with the text field
+        // Send on enter then clear to prepare for next message
+        textField.addActionListener(new ActionListener() {
 
-        public  boolean groupFunction(String cGroupName){
-        if(names.size() ==1){
-         ChatServer.setGroupName(clientGroupName); 
-         }
-
-         if (clientGroupName.equals(ChatServer.getGroupName())){
-            return true;
-         }
-         else {
-             return false;
-                }
-        }
-
-        /**
-         * Services this thread's client by repeatedly requesting a screen name until a
-         * unique one has been submitted, then acknowledges the name and registers the
-         * output stream for the client in a global set, then repeatedly gets inputs and
-         * broadcasts them.
-         */
-        public void run() {
-            try {
-                in = new Scanner(socket.getInputStream());
-                out = new PrintWriter(socket.getOutputStream(), true);
-
-                // Keep requesting a name until we get a unique one.
-                while (true) {
-                    out.println("SUBMITNAME");
-                    name = in.nextLine();
-                    if (name == null) {
-                        return;
-                    }
-                    synchronized (names) {
-                        if (!name.isBlank() && !names.contains(name)) {
-                            names.add(name);
-                            break;
-                        }
-                    }
-                }
-
-
-                // Now that a successful name has been chosen, add the socket's print writer
-                // to the set of all writers so this client can receive broadcast messages.
-                // But BEFORE THAT, let everyone else know that the new person has joined!
-                // the server acknowledges 
-                out.println("NAMEACCEPTED " + name);
-                clientGroupName = in.nextLine();
-                boolean withinGroup = groupFunction(clientGroupName);
-                if(names.size() ==1){
-                    ChatServer.setGroupName(clientGroupName); 
-                }
-                
-                
-                if(withinGroup){
-                    for (PrintWriter writer : writers) {
-                    writer.println("MESSAGE " + name + " has joined");
-                }
-                writers.add(out);
-
-                // ****END OF the initial setup****
-                // Accept messages from this client and broadcast them.
-                while (true) {
-                    String input = in.nextLine();
-                    // the run method returns, terminating the client connection.
-                    if (input.toLowerCase().startsWith("/quit")) {
-                        return;
-                    }
-                    for (PrintWriter writer : writers) {
-                        writer.println("MESSAGE " + name + ": " + input);
-                    }
-                }
-               
-            } else {
-                   out.println("GROUPERROR The meeting room is occupied with another group");
-                   return ;
+            //  Pressing the Return key in the text field triggers the action listener,
+            //  which sends the contents of the text field to the server via the PrintWriter and clears the text field.
+            public void actionPerformed(ActionEvent e) {
+                out.println(textField.getText());
+                textField.setText("");
             }
+        });
+    }
 
-                
-            } catch (Exception e) {
-                System.out.println(e);
-            } finally {
+    // displays a dialog box
+    private String getName() {
+        return JOptionPane.showInputDialog(frame, "Choose a screen name:", "Screen name selection",
+                JOptionPane.PLAIN_MESSAGE);
+    }
 
-                if (out != null) {
-                    writers.remove(out);
-                }
+    private String getGroup(){
+        return JOptionPane.showInputDialog(frame, "Choose a group name:", "group name selection",
+                JOptionPane.PLAIN_MESSAGE);
+    }
+    private void printErrorMessage(){
+        JOptionPane.showMessageDialog(frame, "The room is occupied!", "Error", JOptionPane.ERROR_MESSAGE);
+    }
+
+    private void run() throws IOException {
+        try {
+            
+            // It establishes a connection with the server using a Socket
+            // and sets up the Scanner and PrintWriter for communication with the server.
+            var socket = new Socket(serverAddress, 59001);
+            in = new Scanner(socket.getInputStream());
+            out = new PrintWriter(socket.getOutputStream(), true);
+            
+
+            // continuously reads input from the server using the Scanner
+            while (in.hasNextLine()) {
                 
-                if (name != null) {
-                    System.out.println(name + " is leaving");
-                    names.remove(name);
-                    for (PrintWriter writer : writers) {
-                        writer.println("MESSAGE " + name + " has left");
-                    }
+                var line = in.nextLine();
+                if (line.startsWith("SUBMITNAME")) {
+                    out.println(getName());
+                } else if (line.startsWith("NAMEACCEPTED")) {
+                     out.println(getGroup());
+                    this.frame.setTitle("Chatter - " + line.substring(13));
+                    textField.setEditable(true);
+                } else if (line.startsWith("MESSAGE")) {
+                    messageArea.append(line.substring(8) + "\n");
                 }
-                try {
-                    socket.close();
-                } catch (IOException e) {
+                else if (line.startsWith("GROUPERROR")){
+                  printErrorMessage();
+                  return;
                 }
             }
+        } finally {
+            frame.setVisible(false);
+            frame.dispose();
         }
     }
 
-
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.err.println("Pass the server IP as the sole command line argument");
+            return;
+        }
+        var client = new ChatClient(args[0]);
+        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        client.frame.setVisible(true);
+        client.run();
+    }
 }
