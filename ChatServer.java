eb7a66e53c47fa288a783c5a43272236bd11a95c
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-12-05 11:29:28 +0300

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,3 +1,4 @@
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
@@ -27,71 +28,85 @@ public class ChatServer {
     // The set of all the print writers for all the clients, used for broadcast.
     private static Set<PrintWriter> writers = new HashSet<>();
 
-    private static String serverGroupName ;
+    // Store the server's group name.
+    private static String serverGroupName;
 
+    // Method to get the server's group name
     public static String getGroupName() {
         return serverGroupName;
     }
-    
-    public static void setGroupName(String groupName) {
+// ---------------------------------------------------------------------------------------------------
+
+    // Method to set the server's group name
+    private static void setGroupName(String groupName) {
         ChatServer.serverGroupName = groupName;
     }
+// ---------------------------------------------------------------------------------------------------
+
     public static void main(String[] args) throws Exception {
+
         System.out.println("The chat server is running...");
         var pool = Executors.newFixedThreadPool(500);
 
-        //try-with-resources statement in Java. 
-        // It is a convenient way to ensure that a resource is properly closed after being used, 
-        // even if an exception occurs.
+        // Creating a server socket to listen for client connections on port 59001
+        // This way of writing is a convenient way to ensure that a resource is properly closed after being used.
         try (var listener = new ServerSocket(59001)) {
             while (true) {
 
                 // Whenever a client connects, a new Handler thread is created
                 // and added to the thread pool to handle the client's requests.
                 pool.execute(new Handler(listener.accept()));
+
             }
         }
     }
-
-    /**
-     * The client handler task.
-     * Each Handler instance is responsible for handling a single client connection.
-     */
+// ---------------------------------------------------------------------------------------------------
+    
+    // The client handler task. Each Handler instance is responsible for handling a single client connection.
     private static class Handler implements Runnable {
+
+        // Instance variables for each client connection
         private String name;
-        private String clientGroupName ;
+        private String clientGroupName;
         private Socket socket;
         private Scanner in;
         private PrintWriter out;
         private boolean withinGroup;
 
         /*
-         * Constructs a handler thread, squirreling away the socket. All the interesting
-         * work is done in the run method. Remember the constructor is called from the
-         * server's main method, so this has to be as short as possible.
+          Constructs a handler thread, squirreling away the socket. All the interesting
+          work is done in the run method. Remember the constructor is called from the
+          server's main method, so this has to be as short as possible.
          */
         public Handler(Socket socket) {
             this.socket = socket;
         }
+// ---------------------------------------------------------------------------------------------------
+        
+        // Method to checks if the client belongs to the same group as the server. Returns true if it does, false otherwise.
+        public boolean groupFunction(String cGroupName) {
 
-        public  boolean groupFunction(String cGroupName){
-        if(names.size() ==1){
-         ChatServer.setGroupName(clientGroupName); 
-         }
+            // if the clinet is the first client then set hi group name as the server group name     
+            if (names.size() == 1) {
+                ChatServer.setGroupName(clientGroupName);
+            }
 
-         if (clientGroupName.equals(ChatServer.getGroupName())){
-            return true;
-         }
-         else {
-             return false;
-                }
+            // if the client's group name is equal to server's group then return true  
+            if (clientGroupName.equals(ChatServer.getGroupName())) {
+                return true;
+                
+            } // if the client's group name is not equal to server's group then return false 
+            else {
+                return false;
+            }
         }
-
+// ---------------------------------------------------------------------------------------------------
+        
         /**
-         * Services this thread's client by repeatedly requesting a screen name until a
-         * unique one has been submitted, then acknowledges the name and registers the
-         * output stream for the client in a global set, then repeatedly gets inputs and
-         * broadcasts them.
+         * Services this thread's client by repeatedly requesting a screen name
+         * until a unique one has been submitted, then acknowledges the name and
+         * registers the output stream for the client in a global set, then
+         * repeatedly gets inputs and broadcasts them.
          */
         public void run() {
             try {
@@ -100,80 +115,112 @@ public class ChatServer {
 
                 // Keep requesting a name until we get a unique one.
                 while (true) {
+
+                    // Request the client to submit a name 
                     out.println("SUBMITNAME");
+
+                    // Read the name written by the client 
                     name = in.nextLine();
+
+                    // If the name is null, then end connection with client
                     if (name == null) {
                         return;
                     }
+
+                    // synchronization to ensure thread safety when accessing and modifying the shared names set.
                     synchronized (names) {
+
+                        //Checking if the name is non-empty and not already in use
                         if (!name.isBlank() && !names.contains(name)) {
+
+                            // add the client to the name list
                             names.add(name);
                             break;
                         }
                     }
                 }
 
-
                 // Now that a successful name has been chosen, add the socket's print writer
                 // to the set of all writers so this client can receive broadcast messages.
-                // But BEFORE THAT, let everyone else know that the new person has joined!
+                // but before that, let everyone else know that the new person has joined!
                 // the server acknowledges 
+                // A successful name has been chosen , inform the client
                 out.println("NAMEACCEPTED " + name);
+
+                // Reading the group name written by the client 
                 clientGroupName = in.nextLine();
-                 withinGroup = groupFunction(clientGroupName);
-                if(names.size() ==1){
-                    ChatServer.setGroupName(clientGroupName); 
-                }
-                
-                
-                if(withinGroup){
-                    for (PrintWriter writer : writers) {
-                    writer.println("MESSAGE " + name + " has joined");
-                }
-                writers.add(out);
 
-                // ****END OF the initial setup****
-                // Accept messages from this client and broadcast them.
-                while (true) {
-                    String input = in.nextLine();
-                    // the run method returns, terminating the client connection.
-                    if (input.toLowerCase().startsWith("/quit")) {
-                        return;
-                    }
-                    for (PrintWriter writer : writers) {
-                        writer.println("MESSAGE " + name + ": " + input);
-                    }
-                }
-               
-            } else {
-                   names.remove(name);
-                   out.println("GROUPERROR The meeting room is occupied with another group");
-                   
-            }
+                // Flag to know if the client has joined the group or not
+                withinGroup = groupFunction(clientGroupName);
 
-                
+                // If the client has joined the group 
+                if (withinGroup) {
+
+                    // Lettin everyone in the group know that a new client has joined!
+                    for (PrintWriter writer : writers) {
+                        writer.println("MESSAGE " + name + " has joined");
+                    }
+
+                    // Adding the client print writer    
+                    writers.add(out);
+                    // **** END OF the initial setup ****
+                    // Receiveing messages from the client and broadcasts them to all other connected clients in the group.
+                    while (true) {
+                  
+                        // Reading a message from the client
+                        String input = in.nextLine();
+
+                        // If the client wish to leave, end connection
+                        if (input.toLowerCase().startsWith("/quit")) {
+                            return;
+                        }
+
+                        // Broadcasting the message to all connected clients
+                        for (PrintWriter writer : writers) {
+                            writer.println("MESSAGE " + name + ": " + input);
+                        }
+                    }
+
+                } // If the client has not joined the group 
+                else {
+                    // Removing the client name and informing the client
+                    names.remove(name);
+                    out.println("GROUPERROR The meeting room is occupied with another group");
+                }
+
             } catch (Exception e) {
                 System.out.println(e);
             } finally {
 
                 if (out != null) {
+
+                    // Removing the client print writer  
                     writers.remove(out);
                 }
-                
+
+                // If the client is within the group.
                 if (name != null && withinGroup) {
+
+                    // Printing in the console that the client is leaving.
                     System.out.println(name + " is leaving");
+
+                    // Removing his name from the names list
                     names.remove(name);
+
+                    // Lettin everyone in the group know that a client has leaved!
                     for (PrintWriter writer : writers) {
                         writer.println("MESSAGE " + name + " has left");
                     }
                 }
                 try {
+
+                    // Close the socket to free the resources
                     socket.close();
+
                 } catch (IOException e) {
                 }
             }
         }
     }
-
 
 }
