5e82f902379bc5f095c8f5edb6873fd3f346c402
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-12-07 01:51:51 +0300

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -103,7 +103,7 @@ public class ChatServer {
 // ---------------------------------------------------------------------------------------------------
         
         /**
-         * Services this thread's client by repeatedly requesting a screen name
+         * Services this thread's client by repeatedly requesting a username
          * until a unique one has been submitted, then acknowledges the name, and
          * requesting a group name then registers the output stream for the client in a global set, then
          * repeatedly gets inputs and broadcasts them.
